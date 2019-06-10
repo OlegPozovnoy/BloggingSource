a5092cbf3eb01ff0a0a5edcc9358eb0d6f8a1c49
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2019-06-10 09:21:33 -0400

    changes

--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ const app = express();
 
 // Import our Page Routes
 const pageRoutes = require('./routes/pages');
+const blogsRoutes = require('./routes/blogs');
 
 // Register our Page Routes with our app
 app.use('/', pageRoutes);
+app.use('/blogs', blogsRoutes);
 
 // Export our changes
 module.exports = app;
